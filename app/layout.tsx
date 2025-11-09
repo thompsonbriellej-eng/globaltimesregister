f2542103f5787a22131bcf56f7ad5b584eb0bc71
thompsonbriellej-eng <thompson.brielle.j@gmail.com>
2025-11-09 16:03:30 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./globals.css";
-import { Inter } from "next/font/google";
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
